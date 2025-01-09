dac5784aee6796060017b999708bf4c54aec6141
Xavi Ablaza <xavi@hostari.com>
2025-01-09 04:04:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', async (req, res) => {
   res.send(`This is a C Code Checker, perform a POST /check with JSON body: {"code": encoded-base64-c-code } and you will get a response.`);
 });
 
-const port = 8082;
+const port = parseInt(process.env.PORT) || 8082;
 app.listen(port, () => {
   console.log(`app: listening on port ${port}`);
 });
